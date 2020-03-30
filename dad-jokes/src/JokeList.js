c0ba23e28a37402572f9e5df12a6d892c74a0fd1
aus39360 <aus39360@gmail.com>
2020-03-30 23:23:24 +0300

    joke

---
+++ dad-jokes/src/JokeList.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import './JokeList.css';
+import Joke from './Joke.js';
+import uuid from 'uuid/v4'
+
+class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10
+    };
+    constructor(props) {
+        super(props);
+        this.state = { jokes: []}
+    }
+    async componentDidMount() {
+        let jokes = []
+        while(jokes.length < this.props.numJokesToGet) {
+            let res = await axios.get('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' }})
+            jokes.push({ id: uuid(), text: res.data.joke, votes: 0})
+        }
+        this.setState({ jokes: jokes})
+    }
+    handleVote(id, delta) {
+        this.setState(st =>({
+            jokes: st.jokes.map(j =>
+                j.id === id ? {...j, votes: j.votes + delta} : j
+                )
+        }))
+    }
+    render() {
+        return(
+            <div className='jokeList'>
+                <div className='jokeList-sidebar'>
+                    <h1 className='jokeList-title'><span>Dad</span> Jokes</h1>
+                    <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+                    <button>ADD</button>
+                </div>
+                <div className='jokeList-jokes'>
+                    {this.state.jokes.map(j =>(
+                        <Joke key={j.id} text={j.text} votes={j.votes} upvote={()=> this.handleVote(j.id,1)} dawnvote={()=> this.handleVote(j.id,-1)} />
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default JokeList;
